fa251013936a7fe6326fd710ba6646cc79e344d1
Ty Giacalone <tygiacalone@gmail.com>
2015-07-17 11:55:04 -0700

    Added autocomplete search functionality

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,17 +1,62 @@
-var x = 3;
-
 var socket = io.connect('http://localhost:3700');
 
 window.onload = function() {
 
-    socket.emit('test', { q: 'ham'});
+    var search = document.getElementById('search');
+    var content = document.getElementById('itemlist');
+    var autocomplete = document.getElementById('autocomplete');
 
+    var resultList = [];
     socket.on('results', function (data) {
         console.log(data);
         console.log('results received');
+        //resultList = [];
+        for (var i = 0; i < data.length; i++) {
+            var json = { value: data[i].text};
+            resultList.push(json);
+        }
+
+        console.log(resultList);
+
+        // setup autocomplete function pulling from currencies[] array
+        $('#autocomplete').autocomplete({
+            lookup: resultList,
+            onSelect: function (suggestion) {
+                console.log(resultList);
+                var thehtml = '<strong>Item Name:</strong> ' + suggestion.value;// + ' <br> <strong>Symbol:</strong> ' + suggestion.data;
+                $('#outputcontent').html(thehtml);
+            }
+        });
+        /*
+        var list = '';
         for (var i = 0; i < data.length; i++) {
             var text = data[i].text;
+            list += '<li class="list-unstyled">' + data[i].text + '</li>';
             console.log(text);
         }
+        content.innerHTML = list;
+        */
     });
-}
+
+    var searched = false;
+    document.onkeydown = function (event) {
+        if (autocomplete.value.length > 2 && !searched){
+            var text = autocomplete.value;
+            socket.emit('test', {q: text} );
+            searched = true;
+        }
+
+        if (autocomplete.value.length < 2){
+            var text = autocomplete.value;
+            socket.emit('test', {q: text} );
+            searched = false;
+        }
+
+        if (window.event.keyCode == '13') {
+            var text = autocomplete.value;
+            socket.emit('test', {q: text} );
+            searched = false;
+        }
+    };
+
+};
--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,9 @@ router.get('/bootstrap', function(req, res, next) {
   res.render('indexSmall', { intro: 'Macronutrient and nutrition data for your favorite foods and restaurants' });
 });
 
+/* GET home page. */
+router.get('/search', function(req, res, next) {
+    res.render('search');
+});
 
 module.exports = router;
